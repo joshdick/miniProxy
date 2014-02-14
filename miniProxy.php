95520edd3143868d216b4e6aafc54b7625b2ec31
Tigrouzen <vartanjean7@gmail.com>
2014-02-14 16:33:35 +0100

    Now we can get google, youtube... ;)

--- miniProxy.php
+++ miniProxy.php
@@ -28,6 +28,77 @@ if (!function_exists("getallheaders")) {
 define("PROXY_PREFIX", "http" . (isset($_SERVER['HTTPS']) ? "s" : "") . "://" . $_SERVER["SERVER_NAME"] . ($_SERVER["SERVER_PORT"] != 80 ? ":" . $_SERVER["SERVER_PORT"] : "") . $_SERVER["SCRIPT_NAME"] . "/");
 
 //Makes an HTTP request via cURL, using request data that was passed directly to this script.
+function curl_exec_follow($ch, &$maxredirect = null) {
+  
+  // we emulate a browser here since some websites detect
+  // us as a bot and don't let us do our job
+  $user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5)".
+                " Gecko/20041107 Firefox/1.0";
+  curl_setopt($ch, CURLOPT_USERAGENT, $user_agent );
+
+  $mr = $maxredirect === null ? 5 : intval($maxredirect);
+
+  if (ini_get('open_basedir') == '' && ini_get('safe_mode') == 'Off') {
+
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, $mr);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+  } else {
+    
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+    if ($mr > 0)
+    {
+      $original_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+      $newurl = $original_url;
+      
+      $rch = curl_copy_handle($ch);
+      
+      curl_setopt($rch, CURLOPT_HEADER, true);
+      curl_setopt($rch, CURLOPT_NOBODY, true);
+      curl_setopt($rch, CURLOPT_FORBID_REUSE, false);
+      do
+      {
+        curl_setopt($rch, CURLOPT_URL, $newurl);
+        $header = curl_exec($rch);
+        if (curl_errno($rch)) {
+          $code = 0;
+        } else {
+          $code = curl_getinfo($rch, CURLINFO_HTTP_CODE);
+          if ($code == 301 || $code == 302) {
+            preg_match('/Location:(.*?)\n/', $header, $matches);
+            $newurl = trim(array_pop($matches));
+            
+            // if no scheme is present then the new url is a
+            // relative path and thus needs some extra care
+            if(!preg_match("/^https?:/i", $newurl)){
+              $newurl = $original_url . $newurl;
+            }   
+          } else {
+            $code = 0;
+          }
+        }
+      } while ($code && --$mr);
+      
+      curl_close($rch);
+      
+      if (!$mr)
+      {
+        if ($maxredirect === null)
+        trigger_error('Too many redirects.', E_USER_WARNING);
+        else
+        $maxredirect = 0;
+        
+        return false;
+      }
+      curl_setopt($ch, CURLOPT_URL, $newurl);
+    }
+  }
+  return curl_exec($ch);
+}
+
 function makeRequest($url) {
 
   //Tell cURL to make the request using the brower's user-agent if there is one, or a fallback user-agent otherwise.
@@ -97,7 +168,7 @@ function makeRequest($url) {
   curl_setopt($ch, CURLOPT_URL, $url);
 
   //Make the request.
-  $response = curl_exec($ch);
+  $response = curl_exec_follow($ch);
   $responseInfo = curl_getinfo($ch);
   $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
   curl_close($ch);
