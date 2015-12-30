2ac6f8b0f1c865a1d4478286afda3b82a1fb31f2
Josh Dick <josh@joshdick.net>
2015-12-30 00:53:15 -0500

    Remove redundant/unused code path for prepending "http:" to "//" URLs

--- miniProxy.php
+++ miniProxy.php
@@ -183,20 +183,18 @@ function proxifyCSS($css, $baseURL) {
 $url = substr($_SERVER["REQUEST_URI"], strlen($_SERVER["SCRIPT_NAME"]) + 1);
 if (empty($url)) {
     die("<html><head><title>miniProxy</title></head><body><h1>Welcome to miniProxy!</h1>miniProxy can be directly invoked like this: <a href=\"" . PROXY_PREFIX . "http://example.net/\">" . PROXY_PREFIX . "http://example.net/</a><br /><br />Or, you can simply enter a URL below:<br /><br /><form onsubmit=\"window.location.href='" . PROXY_PREFIX . "' + document.getElementById('site').value; return false;\"><input id=\"site\" type=\"text\" size=\"50\" /><input type=\"submit\" value=\"Proxy It!\" /></form></body></html>");
-} else if (strpos($url, "//") === 0) {
-    //Assume that any supplied URLs starting with // are HTTP URLs.
-    $url = "http:" . $url;
 } else if (strpos($url, ":/") !== strpos($url, "://")) {
     //Work around the fact that some web servers (e.g. IIS 8.5) change double slashes appearing in the URL to a single slash.
     //See https://github.com/joshdick/miniProxy/pull/14
     $pos = strpos($url, ":/");
     $url = substr_replace($url, "://", $pos, strlen(":/"));
 }
-
 $scheme = parse_url($url, PHP_URL_SCHEME);
 if (empty($scheme)) {
-    //Assume that any supplied URLs without a scheme are HTTP URLs.
-    $url = "http://" . $url;
+  //Assume that any supplied URLs starting with // are HTTP URLs.
+  if (strpos($url, "//") === 0) {
+    $url = "http:" . $url;
+  }
 } else if (!preg_match("/^https?$/i", $scheme)) {
     die('Error: Detected a "' . $scheme . '" URL. miniProxy exclusively supports http[s] URLs.');
 }
