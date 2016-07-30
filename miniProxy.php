f43a4b4d23ce80ae52ef7cfded9fed3284eb12ff
Josh Dick <josh@joshdick.net>
2016-07-30 14:17:10 -0400

    Don't proxify magnet links.
    Fixes #40.

--- miniProxy.php
+++ miniProxy.php
@@ -303,7 +303,7 @@ if (stripos($contentType, "text/html") !== false) {
   foreach($proxifyAttributes as $attrName) {
     foreach($xpath->query('//*[@' . $attrName . ']') as $element) { //For every element with the given attribute...
       $attrContent = $element->getAttribute($attrName);
-      if ($attrName == "href" && (stripos($attrContent, "javascript:") === 0 || stripos($attrContent, "mailto:") === 0)) continue;
+      if ($attrName == "href" && preg_match("/^(javascript|magnet|mailto):/i", $attrContent)) continue;
       $attrContent = rel2abs($attrContent, $url);
       $attrContent = PROXY_PREFIX . $attrContent;
       $element->setAttribute($attrName, $attrContent);
