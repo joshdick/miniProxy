35206afbb2c9da9576ced35522673eb7ce3c05e2
Josh Dick <josh@joshdick.net>
2020-04-26 20:15:18 -0400

    feat: Blacklist
    Closes #121.

--- miniProxy.php
+++ miniProxy.php
@@ -7,16 +7,29 @@ miniProxy is licensed under the GNU GPL v3 <http://www.gnu.org/licenses/gpl.html
 
 /****************************** START CONFIGURATION ******************************/
 
+//NOTE: If a given URL matches a pattern in both $whitelistPatterns and $blacklistPatterns,
+//that URL will be treated as blacklisted.
+
 //To allow proxying any URL, set $whitelistPatterns to an empty array (the default).
 //To only allow proxying of specific URLs (whitelist), add corresponding regular expressions
-//to the $whitelistPatterns array. Enter the most specific patterns possible, to prevent possible abuse.
+//to the $whitelistPatterns array. To prevent possible abuse, enter the narrowest/most-specific patterns possible.
 //You can optionally use the "getHostnamePattern()" helper function to build a regular expression that
 //matches all URLs for a given hostname.
-$whitelistPatterns = array(
-  //Usage example: To support any URL at example.net, including sub-domains, uncomment the
+$whitelistPatterns = [
+  //Usage example: To whitelist any URL at example.net, including sub-domains, uncomment the
   //line below (which is equivalent to [ @^https?://([a-z0-9-]+\.)*example\.net@i ]):
   //getHostnamePattern("example.net")
-);
+];
+
+//To disallow proxying of specific URLs (blacklist), add corresponding regular expressions
+//to the $blacklistPatterns array. To prevent possible abuse, enter the broadest/least-specific patterns possible.
+//You can optionally use the "getHostnamePattern()" helper function to build a regular expression that
+//matches all URLs for a given hostname.
+$blacklistPatterns = [
+  //Usage example: To blacklist any URL at example.net, including sub-domains, uncomment the
+  //line below (which is equivalent to [ @^https?://([a-z0-9-]+\.)*example\.net@i ]):
+  //getHostnamePattern("example.net")
+];
 
 //To enable CORS (cross-origin resource sharing) for proxied sites, set $forceCORS to true.
 $forceCORS = false;
@@ -49,18 +62,44 @@ foreach($requiredExtensions as $requiredExtension) {
   }
 }
 
-//Helper function for use inside $whitelistPatterns.
+//Helper function for use inside $whitelistPatterns/$blacklistPatterns.
 //Returns a regex that matches all HTTP[S] URLs for a given hostname.
 function getHostnamePattern($hostname) {
   $escapedHostname = str_replace(".", "\.", $hostname);
   return "@^https?://([a-z0-9-]+\.)*" . $escapedHostname . "@i";
 }
 
+//Helper function that determines whether to allow proxying of a given URL.
+function isValidURL($url) {
+  //Validates a URL against the whitelist.
+  function passesWhitelist($url) {
+    if (count($GLOBALS['whitelistPatterns']) === 0) return true;
+    foreach ($GLOBALS['whitelistPatterns'] as $pattern) {
+      if (preg_match($pattern, $url)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  //Validates a URL against the blacklist.
+  function passesBlacklist($url) {
+    foreach ($GLOBALS['blacklistPatterns'] as $pattern) {
+      if (preg_match($pattern, $url)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return passesWhitelist($url) && passesBlacklist($url);
+}
+
 //Helper function used to removes/unset keys from an associative array using case insensitive matching
 function removeKeys(&$assoc, $keys2remove) {
   $keys = array_keys($assoc);
-  $map = array();
-  $removedKeys = array();
+  $map = [];
+  $removedKeys = [];
   foreach ($keys as $key) {
     $map[strtolower($key)] = $key;
   }
@@ -77,7 +116,7 @@ function removeKeys(&$assoc, $keys2remove) {
 if (!function_exists("getallheaders")) {
   //Adapted from http://www.php.net/manual/en/function.getallheaders.php#99814
   function getallheaders() {
-    $result = array();
+    $result = [];
     foreach($_SERVER as $key => $value) {
       if (substr($key, 0, 5) == "HTTP_") {
         $key = str_replace(" ", "-", ucwords(strtolower(str_replace("_", " ", substr($key, 5)))));
@@ -113,19 +152,22 @@ function makeRequest($url) {
   $browserRequestHeaders = getallheaders();
 
   //...but let cURL set some headers on its own.
-  $removedHeaders = removeKeys($browserRequestHeaders, array(
-    "Accept-Encoding", //Throw away the browser's Accept-Encoding header if any and let cURL make the request using gzip if possible.
-    "Content-Length",
-    "Host",
-    "Origin"
-  ));
+  $removedHeaders = removeKeys(
+    $browserRequestHeaders,
+    [
+      "Accept-Encoding", //Throw away the browser's Accept-Encoding header if any and let cURL make the request using gzip if possible.
+      "Content-Length",
+      "Host",
+      "Origin"
+    ]
+  );
 
   $removedHeaders = array_map("strtolower", $removedHeaders);
 
   curl_setopt($ch, CURLOPT_ENCODING, "");
   //Transform the associative array from getallheaders() into an
   //indexed array of header strings to be passed to cURL.
-  $curlRequestHeaders = array();
+  $curlRequestHeaders = [];
   foreach ($browserRequestHeaders as $name => $value) {
     $curlRequestHeaders[] = $name . ": " . $value;
   }
@@ -151,7 +193,7 @@ function makeRequest($url) {
       //across different server environments.
       //More info here: http://stackoverflow.com/questions/8899239/http-raw-post-data-not-being-populated-after-upgrade-to-php-5-3
       //If the miniProxyFormAction field appears in the POST data, remove it so the destination server doesn't receive it.
-      $postData = Array();
+      $postData = [];
       parse_str(file_get_contents("php://input"), $postData);
       if (isset($postData["miniProxyFormAction"])) {
         unset($postData["miniProxyFormAction"]);
@@ -183,7 +225,7 @@ function makeRequest($url) {
   $responseHeaders = substr($response, 0, $headerSize);
   $responseBody = substr($response, $headerSize);
 
-  return array("headers" => $responseHeaders, "body" => $responseBody, "responseInfo" => $responseInfo);
+  return ["headers" => $responseHeaders, "body" => $responseBody, "responseInfo" => $responseInfo];
 }
 
 //Converts relative URLs to absolute ones, given a base URL.
@@ -205,7 +247,7 @@ function rel2abs($rel, $base) {
     $auth .= "@";
   }
   $abs = "$auth$host$port$path/$rel"; //Dirty absolute URL
-  for ($n = 1; $n > 0; $abs = preg_replace(array("#(/\.?/)#", "#/(?!\.\.)[^/]+/\.\./#"), "/", $abs, -1, $n)) {} //Replace '//' or '/./' or '/foo/../' with '/'
+  for ($n = 1; $n > 0; $abs = preg_replace(["#(/\.?/)#", "#/(?!\.\.)[^/]+/\.\./#"], "/", $abs, -1, $n)) {} //Replace '//' or '/./' or '/foo/../' with '/'
   return $scheme . "://" . $abs; //Absolute URL is ready.
 }
 
@@ -263,7 +305,7 @@ if (isset($_POST["miniProxyFormAction"])) {
   $url = $_POST["miniProxyFormAction"];
   unset($_POST["miniProxyFormAction"]);
 } else {
-  $queryParams = Array();
+  $queryParams = [];
   parse_str($_SERVER["QUERY_STRING"], $queryParams);
   //If the miniProxyFormAction field appears in the query string, make $url start with its value, and rebuild the the query string without it.
   if (isset($queryParams["miniProxyFormAction"])) {
@@ -296,15 +338,7 @@ if (empty($scheme)) {
     die('Error: Detected a "' . $scheme . '" URL. miniProxy exclusively supports http[s] URLs.');
 }
 
-//Validate the requested URL against the whitelist.
-$urlIsValid = count($whitelistPatterns) === 0;
-foreach ($whitelistPatterns as $pattern) {
-  if (preg_match($pattern, $url)) {
-    $urlIsValid = true;
-    break;
-  }
-}
-if (!$urlIsValid) {
+if (!isValidURL($url)) {
   die("Error: The requested URL was disallowed by the server administrator.");
 }
 
@@ -420,7 +454,7 @@ if (stripos($contentType, "text/html") !== false) {
     $element->setAttribute("srcset", proxifySrcset($element->getAttribute("srcset"), $url));
   }
   //Proxify any of these attributes appearing in any tag.
-  $proxifyAttributes = array("href", "src");
+  $proxifyAttributes = ["href", "src"];
   foreach($proxifyAttributes as $attrName) {
     foreach($xpath->query("//*[@" . $attrName . "]") as $element) { //For every element with the given attribute...
       $attrContent = $element->getAttribute($attrName);
